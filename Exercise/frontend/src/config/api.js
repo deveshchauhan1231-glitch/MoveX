42bcb8179f7179d2bf89a28a979c5603173702c3
deveshchauhan1231-glitch <deveshchauhan1.2.3.1@gmail.com>
2026-04-12 02:27:09 +0530

    Update api.js

--- Exercise/frontend/src/config/api.js
+++ Exercise/frontend/src/config/api.js
@@ -1,3 +1,22 @@
+import axios from "axios";
+import supabase from "./supabase.js";
+
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+
+axios.interceptors.request.use(async (config) => {
+  const requestUrl = config.url || "";
+
+  if (BACKEND_URL && requestUrl.startsWith(BACKEND_URL)) {
+    const { data } = await supabase.auth.getSession();
+    const accessToken = data.session?.access_token;
+
+    if (accessToken) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${accessToken}`;
+    }
+  }
+
+  return config;
+});
 
 export { BACKEND_URL };
