2f42008f450b7eb99b6dfdee7c1ff0c771966372
deveshchauhan1231-glitch <deveshchauhan1.2.3.1@gmail.com>
2026-04-12 11:48:36 +0530

    Update exerciseDbRoutes.js

--- Exercise/backend/src/routes/exerciseDbRoutes.js
+++ Exercise/backend/src/routes/exerciseDbRoutes.js
@@ -1,6 +1,6 @@
 import express from "express"
 import exerciseDbController from "../controllers/exerciseDbController.js";
-const router=express();
+const router = express.Router()
 router.get("/getAllExercises",exerciseDbController.getAllExercises);
 router.get("/getThatExercise/:name",exerciseDbController.getThatExercise);
 router.get("/getExerciseByFilter/:filter",exerciseDbController.getExerciseByFilter);
