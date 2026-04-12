cc9bee2f9fca060dded14b2cc49af4d61104a2a8
deveshchauhan1231-glitch <deveshchauhan1.2.3.1@gmail.com>
2026-04-12 11:47:59 +0530

    Update authRoutes.js

--- Exercise/backend/src/routes/authRoutes.js
+++ Exercise/backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import authControllers from "../controllers/authController.js";
 import authMiddleware from "../middleware/authMiddleware.js";
 
-const router = express.Router(); // ✅ fix here
+const router = express.Router(); 
 
 router.post('/complete-profile', authMiddleware, authControllers.completeProfileAfterVerification);
 
