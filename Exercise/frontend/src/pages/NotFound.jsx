89c597d35418d98543206ad1350dc50ee45ffb2f
deveshchauhan1231-glitch <deveshchauhan1.2.3.1@gmail.com>
2026-04-12 19:57:22 +0530

    Update NotFound.jsx

--- Exercise/frontend/src/pages/NotFound.jsx
+++ Exercise/frontend/src/pages/NotFound.jsx
@@ -15,7 +15,7 @@ const NotFound = () => {
       background: "#f8f9fa",
       color: "#333"
     }}>
-      <h1 style={{ fontSize: "8rem", margin: 0 }}>404</h1>
+      <h1 style={{ fontSize: "8rem", margin: 0 , color:"#2bab6f"}}>404</h1>
       <h2 style={{ margin: "1rem 0" }}>Page Not Found</h2>
       <p>The page you're looking for doesn't exist.</p>
       <button style={{background:"#2bab6f"}} onClick={()=>navigate("/")}>Go back to Home</button>
